2270829ec7bf84ebf8f550e2945bcf816fce57ca
Aayan Khan <48133449+Aayan107@users.noreply.github.com>
2021-10-17 18:03:20 +0530

    Initial Commit

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<!-- navbar --> 
+<link rel="stylesheet" href="navbar.css">
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400&display=swap'); </style> 
+      <nav class=" navbar navbar-expand-lg navbar-light">
+      <a style="color:#333; font-size: 25px; font-style: Italic ; font-weight: 700; font-family: 'Noto Sans', sans-serif; 	letter-spacing: 0.1px; margin-left:15px; color:#FFFFFF" class="navbar-brand" href="index.php">Aayan's Basic Banking System</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+			
+              <li class="nav-item">
+              <a style="color: #000000; font-size: 17px; font-weight: 400; font-family: 'Noto Sans', sans-serif; letter-spacing: 0.1px; color:#FFFFFF" class="nav-link" href="index.php">Home</a>
+              </li>
+              
+              <li class="nav-item">
+              <a style="color: #000000; font-size: 17px; font-weight: 400; font-family: 'Noto Sans', sans-serif; letter-spacing: 0.1px; color:#FFFFFF" class="nav-link" href="transfermoney.php">Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a style="color: #000000; font-size: 17px; font-weight: 400; font-family: 'Noto Sans', sans-serif; letter-spacing: 0.1px; color:#FFFFFF" class="nav-link" href="transactionhistory.php">Transaction History</a>
+              </li>
+          </div>
+       </nav>
